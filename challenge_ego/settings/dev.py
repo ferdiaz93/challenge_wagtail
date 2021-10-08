1b58cb3d84dafb5889850793ab24da486a691b0d
ferdiaz93 <fernandodiaz1209@gmail.com>
2021-10-08 16:04:15 -0300

    initial app

---
+++ challenge_ego/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-iofs*0&-rwvzc11^=w!9y!@)lp_qd3k17-6$(l=*p$ryc+=kh5'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
